1daf079a61f26c24abaed156b33fd8e48c4cef80
JoelH05 <j_h_olla@hotmail.com>
2017-03-03 13:58:40 -0500

    Completed lab

--- Icecream/Icecream.swift
+++ Icecream/Icecream.swift
@@ -10,12 +10,30 @@ class Icecream {
     
     // 1. Create the Dictionary here. The name of the variable should be favoriteFlavorsOfIceCream
     
+    var favoriteFlavorsOfIceCream: [String : String] = [
+        "Joe" : "Peanut Butter and Chocolate",
+        "Tim" : "Natural Vanilla",
+        "Sophie" : "Mexican Chocolate",
+        "Deniz" : "Natural Vanilla",
+        "Tom" : "Mexican Chocolate",
+        "Jim" : "Natural Vanilla",
+        "Susan" : "Cookies 'N' Cream"
+    ]
     
     
     
     
     // 2.
     
+    func names(forFlavor flavor: String) -> [String] {
+        var namesForGivenFlavor: [String] = []
+        for (names, favoriteFlavor) in favoriteFlavorsOfIceCream {
+            if flavor == favoriteFlavor {
+                namesForGivenFlavor.append(names)
+            }
+        }
+        return namesForGivenFlavor
+    }
     
     
     
@@ -23,7 +41,15 @@ class Icecream {
     
     // 3.
     
-    
+    func count(forFlavor flavor: String) -> Int {
+        var count: Int = 0
+        for favoriteFlavor in favoriteFlavorsOfIceCream.values {
+            if favoriteFlavor == flavor {
+                count += 1
+            }
+        }
+        return count
+    }
     
     
     
@@ -31,7 +57,18 @@ class Icecream {
     
     // 4.
    
-    
+    func flavor(forPerson person: String) -> String? {
+        var personFlavor: String? = ""
+        
+        for (keys, values) in favoriteFlavorsOfIceCream {
+            if person == keys {
+                personFlavor = values
+            } else {
+                personFlavor = nil
+            }
+        }
+        return personFlavor
+    }
     
     
     
@@ -39,6 +76,18 @@ class Icecream {
     
     // 5.
    
+    func replace(flavor: String, forPerson person: String) -> Bool {
+        var personChanged: Bool = true
+        for keys in favoriteFlavorsOfIceCream.keys {
+            if person == keys {
+                favoriteFlavorsOfIceCream[person] = flavor
+                personChanged = true
+            } else {
+                personChanged = false
+            }
+        }
+        return personChanged
+    }
     
     
     
@@ -48,7 +97,18 @@ class Icecream {
     // 6.
     
     
-    
+    func remove(person: String) -> Bool {
+        var wasComing = true
+        for keys in favoriteFlavorsOfIceCream.keys {
+            if person == keys {
+                favoriteFlavorsOfIceCream[keys] = nil
+                wasComing = true
+            } else {
+                wasComing = false
+            }
+        }
+        return wasComing
+    }
     
     
     
@@ -57,7 +117,9 @@ class Icecream {
     // 7.
     
     
-    
+    func numberOfAttendees() -> Int {
+        return favoriteFlavorsOfIceCream.keys.count
+    }
     
     
     
@@ -66,7 +128,18 @@ class Icecream {
     // 8.
     
     
-    
+    func add(person: String, withFlavor flavor: String) -> Bool {
+        var personAdded: Bool = true
+        for key in favoriteFlavorsOfIceCream.keys {
+            if person != key {
+                favoriteFlavorsOfIceCream["\(person)"] = "\(flavor)"
+                personAdded = true
+            } else {
+                personAdded = false
+            }
+        }
+       return personAdded
+    }
     
     
     
@@ -76,7 +149,24 @@ class Icecream {
     // 9.
     
     
-    
+    func attendeeList() -> String {
+        
+        var alphabeticalOrder = ""
+        let allNames = Array(favoriteFlavorsOfIceCream.keys).sorted()
+        
+        for name in allNames {
+            if let personFlavor = favoriteFlavorsOfIceCream[name] {
+                if alphabeticalOrder.isEmpty {
+                    alphabeticalOrder.append("\(name) likes \(personFlavor)")
+                } else {
+                    alphabeticalOrder.append("\n\(name) likes \(personFlavor)")
+                }
+            }
+        }
+        
+        print(alphabeticalOrder)
+        return alphabeticalOrder
+    }
     
     
     
